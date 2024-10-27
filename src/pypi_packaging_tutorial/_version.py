6d0f75cb459990fe29e9f82b2144b2a2c1528eeb
k4144 <github.k4144@gmail.com>
2024-10-27 17:57:16 +0100

    fix coverage

--- src/pypi_packaging_tutorial/_version.py
+++ src/pypi_packaging_tutorial/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev8+g0a9a465.d20241027'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev8', 'g0a9a465.d20241027')
+__version__ = version = '0.1.1.dev14+g83d3102.d20241027'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev14', 'g83d3102.d20241027')
