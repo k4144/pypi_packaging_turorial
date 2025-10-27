3821965da465f3588868ee0259b7d30eaa56f7a8
k4144 <github.k4144@gmail.com>
2025-10-27 01:27:44 +0100

    *

--- src/pypi_packaging_tutorial/__init__.py
+++ src/pypi_packaging_tutorial/__init__.py
@@ -1,6 +1,6 @@
 from .divide.divide_by_three import divide_by_three as divide_by_three
 from .multiply.multiply_by_three import multiply_by_three as multiply_by_three
-from .example import add_one
-from .core import add_two
+from .example import add_one as add_one
+from .core import add_two as add_two
 
 # from .tests.test import test_method as test_method
